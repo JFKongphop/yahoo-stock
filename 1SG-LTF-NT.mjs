02311250fc6a92f9ba1e99e0bb11af68addc73e4
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2024-03-01 22:47:15 +0700

    change stock

--- 1SG-LTF-NT.mjs
+++ 1SG-LTF-NT.mjs
@@ -1,308 +1,308 @@
 export default [
   {
-    "securityID": 219143,
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240101",
-    "AccReturn": 3.28
-  },
-  {
-    "securityID": 219143,
-    "NAV": 33.032900000000000,
+    "AccReturn": 1.88
+},
+{
+    "securityID": 103351,
+    "NAV": 9.328400000000000,
     "NAVDate": "20240102",
-    "AccReturn": 4.17
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.841000000000000,
+    "AccReturn": 2.97
+},
+{
+    "securityID": 103351,
+    "NAV": 9.296000000000000,
     "NAVDate": "20240103",
-    "AccReturn": 3.57
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.911700000000000,
+    "AccReturn": 2.61
+},
+{
+    "securityID": 103351,
+    "NAV": 9.350800000000000,
     "NAVDate": "20240104",
-    "AccReturn": 3.79
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.703800000000000,
+    "AccReturn": 3.22
+},
+{
+    "securityID": 103351,
+    "NAV": 9.325500000000000,
     "NAVDate": "20240105",
-    "AccReturn": 3.14
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 2.94
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240106",
-    "AccReturn": 3.14
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 2.94
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240107",
-    "AccReturn": 3.14
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.470200000000000,
+    "AccReturn": 2.94
+},
+{
+    "securityID": 103351,
+    "NAV": 9.272900000000000,
     "NAVDate": "20240108",
-    "AccReturn": 2.40
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.454000000000000,
+    "AccReturn": 2.36
+},
+{
+    "securityID": 103351,
+    "NAV": 9.222700000000000,
     "NAVDate": "20240109",
-    "AccReturn": 2.35
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.376700000000000,
+    "AccReturn": 1.80
+},
+{
+    "securityID": 103351,
+    "NAV": 9.236300000000000,
     "NAVDate": "20240110",
-    "AccReturn": 2.10
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.266400000000000,
+    "AccReturn": 1.95
+},
+{
+    "securityID": 103351,
+    "NAV": 9.195200000000000,
     "NAVDate": "20240111",
-    "AccReturn": 1.76
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.342300000000000,
+    "AccReturn": 1.50
+},
+{
+    "securityID": 103351,
+    "NAV": 9.255500000000000,
     "NAVDate": "20240112",
-    "AccReturn": 1.99
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 2.16
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240113",
-    "AccReturn": 1.99
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 2.16
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240114",
-    "AccReturn": 1.99
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.165600000000000,
+    "AccReturn": 2.16
+},
+{
+    "securityID": 103351,
+    "NAV": 9.207700000000000,
     "NAVDate": "20240115",
-    "AccReturn": 1.44
-  },
-  {
-    "securityID": 219143,
-    "NAV": 32.029100000000000,
+    "AccReturn": 1.64
+},
+{
+    "securityID": 103351,
+    "NAV": 9.185600000000000,
     "NAVDate": "20240116",
-    "AccReturn": 1.01
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.531900000000000,
+    "AccReturn": 1.39
+},
+{
+    "securityID": 103351,
+    "NAV": 9.034900000000000,
     "NAVDate": "20240117",
-    "AccReturn": -0.56
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.420500000000000,
+    "AccReturn": -0.27
+},
+{
+    "securityID": 103351,
+    "NAV": 9.029600000000000,
     "NAVDate": "20240118",
-    "AccReturn": -0.91
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.611300000000000,
+    "AccReturn": -0.33
+},
+{
+    "securityID": 103351,
+    "NAV": 9.079500000000000,
     "NAVDate": "20240119",
-    "AccReturn": -0.31
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 0.22
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240120",
-    "AccReturn": -0.31
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 0.22
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240121",
-    "AccReturn": -0.31
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.265300000000000,
+    "AccReturn": 0.22
+},
+{
+    "securityID": 103351,
+    "NAV": 8.996000000000000,
     "NAVDate": "20240122",
-    "AccReturn": -1.40
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.035200000000000,
+    "AccReturn": -0.70
+},
+{
+    "securityID": 103351,
+    "NAV": 8.893200000000000,
     "NAVDate": "20240123",
-    "AccReturn": -2.13
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.574100000000000,
+    "AccReturn": -1.84
+},
+{
+    "securityID": 103351,
+    "NAV": 9.072000000000000,
     "NAVDate": "20240124",
-    "AccReturn": -0.43
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.484900000000000,
+    "AccReturn": 0.14
+},
+{
+    "securityID": 103351,
+    "NAV": 9.070200000000000,
     "NAVDate": "20240125",
-    "AccReturn": -0.71
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.198300000000000,
+    "AccReturn": 0.12
+},
+{
+    "securityID": 103351,
+    "NAV": 9.019200000000000,
     "NAVDate": "20240126",
-    "AccReturn": -1.61
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": -0.44
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240127",
-    "AccReturn": -1.61
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": -0.44
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240128",
-    "AccReturn": -1.61
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.392500000000000,
+    "AccReturn": -0.44
+},
+{
+    "securityID": 103351,
+    "NAV": 9.111500000000000,
     "NAVDate": "20240129",
-    "AccReturn": -1.00
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.288700000000000,
+    "AccReturn": 0.57
+},
+{
+    "securityID": 103351,
+    "NAV": 9.079200000000000,
     "NAVDate": "20240130",
-    "AccReturn": -1.33
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.175400000000000,
+    "AccReturn": 0.22
+},
+{
+    "securityID": 103351,
+    "NAV": 9.059900000000000,
     "NAVDate": "20240131",
-    "AccReturn": -1.68
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.250400000000000,
+    "AccReturn": 0.00
+},
+{
+    "securityID": 103351,
+    "NAV": 9.087000000000000,
     "NAVDate": "20240201",
-    "AccReturn": -1.45
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.651400000000000,
+    "AccReturn": 0.30
+},
+{
+    "securityID": 103351,
+    "NAV": 9.228500000000000,
     "NAVDate": "20240202",
-    "AccReturn": -0.18
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 1.87
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240203",
-    "AccReturn": -0.18
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 1.87
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240204",
-    "AccReturn": -0.18
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.627000000000000,
+    "AccReturn": 1.87
+},
+{
+    "securityID": 103351,
+    "NAV": 9.238000000000000,
     "NAVDate": "20240205",
-    "AccReturn": -0.26
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.954200000000000,
+    "AccReturn": 1.97
+},
+{
+    "securityID": 103351,
+    "NAV": 9.326800000000000,
     "NAVDate": "20240206",
-    "AccReturn": 0.77
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.994000000000000,
+    "AccReturn": 2.95
+},
+{
+    "securityID": 103351,
+    "NAV": 9.350500000000000,
     "NAVDate": "20240207",
-    "AccReturn": 0.90
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.721500000000000,
+    "AccReturn": 3.21
+},
+{
+    "securityID": 103351,
+    "NAV": 9.249500000000000,
     "NAVDate": "20240208",
-    "AccReturn": 0.04
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.718600000000000,
+    "AccReturn": 2.10
+},
+{
+    "securityID": 103351,
+    "NAV": 9.234300000000000,
     "NAVDate": "20240209",
-    "AccReturn": 0.03
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 1.93
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240210",
-    "AccReturn": 0.03
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 1.93
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240211",
-    "AccReturn": 0.03
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.760600000000000,
+    "AccReturn": 1.93
+},
+{
+    "securityID": 103351,
+    "NAV": 9.236500000000000,
     "NAVDate": "20240212",
-    "AccReturn": 0.16
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.765900000000000,
+    "AccReturn": 1.95
+},
+{
+    "securityID": 103351,
+    "NAV": 9.256000000000000,
     "NAVDate": "20240213",
-    "AccReturn": 0.18
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.599700000000000,
+    "AccReturn": 2.17
+},
+{
+    "securityID": 103351,
+    "NAV": 9.258000000000000,
     "NAVDate": "20240214",
-    "AccReturn": -0.35
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.632500000000000,
+    "AccReturn": 2.19
+},
+{
+    "securityID": 103351,
+    "NAV": 9.313900000000000,
     "NAVDate": "20240215",
-    "AccReturn": -0.24
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.672000000000000,
+    "AccReturn": 2.81
+},
+{
+    "securityID": 103351,
+    "NAV": 9.321500000000000,
     "NAVDate": "20240216",
-    "AccReturn": -0.12
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 2.89
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240217",
-    "AccReturn": -0.12
-  },
-  {
-    "securityID": 219143,
+    "AccReturn": 2.89
+},
+{
+    "securityID": 103351,
     "NAV": null,
     "NAVDate": "20240218",
-    "AccReturn": -0.12
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.775900000000000,
+    "AccReturn": 2.89
+},
+{
+    "securityID": 103351,
+    "NAV": 9.348600000000000,
     "NAVDate": "20240219",
-    "AccReturn": 0.21
-  },
-  {
-    "securityID": 219143,
-    "NAV": 31.730200000000000,
+    "AccReturn": 3.19
+},
+{
+    "securityID": 103351,
+    "NAV": 9.346400000000000,
     "NAVDate": "20240220",
-    "AccReturn": 0.06
-  },
+    "AccReturn": 3.17
+}
 ]
